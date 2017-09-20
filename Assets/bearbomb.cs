e7751ede7b1814cda67f1e2b0b8d26e3a0b6e0f4
Unknown <sgh284@nyu.edu>
2017-09-19 23:25:41 -0400

    Wenesday 9/20

---
+++ Assets/bearbomb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bearbomb : MonoBehaviour {
+
+    public int life;
+
+    // Use this for initialization
+    void Start () {
+        life = 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Slerp(transform.position, GameObject.Find("Player").GetComponent<Transform>().position, .01f);
+        transform.position+=new Vector3(Random.Range(.1f,4),Mathf.Cos(Time.deltaTime),0);
+
+        life -= 1;
+        if (life == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+   
+
+}
